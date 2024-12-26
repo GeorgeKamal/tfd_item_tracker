328cef40d4bf07fd8b2c87c7e204d82728289ce0
GeorgeKamal <83922448+GeorgeKamal@users.noreply.github.com>
2024-12-26 15:46:48 +0200

    Added States

--- lib/components/asset_details.dart
+++ lib/components/asset_details.dart
@@ -1,17 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:tfd_item_tracker/models/asset.interface.dart';
 import 'package:tfd_item_tracker/models/descendant.model.dart';
+import 'package:tfd_item_tracker/utils/utils.dart';
 
-class AssetDetails extends StatelessWidget {
-  //TODO - handle patterns data
+class AssetDetails extends StatefulWidget {
   final Asset asset;
   
   const AssetDetails({required this.asset, super.key});
 
+  @override
+  State<StatefulWidget> createState() => AssetDetailsState();
+  
+}
+
+class AssetDetailsState extends State<AssetDetails> {
+  //TODO - handle patterns data
+
+  late List<int> count;
+  late List<bool> owned;
+
+  @override
+  void initState() {
+    super.initState();
+    count = List.filled(widget.asset.getParts.length, 0);
+    owned = List.filled(widget.asset.getParts.length, false);
+  }
+
   List<Widget> generateRows(BuildContext context, bool isPortrait) {
     double screenWidth = MediaQuery.of(context).size.width;
 
-    List<Widget> rows = List.generate(asset.getParts.length, (int index) =>
+    List<Widget> rows = List.generate(widget.asset.getParts.length, (int index) =>
       // Expanded(
       //   child: 
         Column(
@@ -31,14 +51,14 @@ class AssetDetails extends StatelessWidget {
                         Row(
                           children: [
                             SizedBox(width: 16),
-                            SizedBox(width: screenWidth * (isPortrait? 0.6: 0.45), child: Text(asset.getParts[index])),
+                            SizedBox(width: screenWidth * (isPortrait? 0.6: 0.45), child: Text(widget.asset.getParts[index])),
                           ],
                         ),
                         generateStateHolderWidget(index)
                       ],
                     ),
                   ),
-                  if(index < asset.getParts.length - 1)
+                  if(index < widget.asset.getParts.length - 1)
                     Divider(height: 20,)
                 ],
               )
@@ -61,19 +81,40 @@ class AssetDetails extends StatelessWidget {
 
     Widget stateHolder = Row(
       children: [
-        IconButton(onPressed: () {print("remove $index");}, icon: Icon(Icons.remove)),
-        Text("0"), 
-        IconButton(onPressed: () {print("add $index");}, icon: Icon(Icons.add)), 
+        IconButton(onPressed: () {
+          if(count[index] > 0) {
+            setState(() {
+              count[index]--;
+              // count[index] = max(count[index] - 1, 0);
+            });
+            Utils.alert(context, "Saved!");
+          }
+        }, icon: Icon(Icons.remove)),
+        Text(count[index].toString()), 
+        IconButton(onPressed: () {
+          if(count[index] < 5) {
+            setState(() {
+              count[index]++;
+              // count[index] = min(count[index] + 1, 5);
+            });
+            Utils.alert(context, "Saved!");
+          }
+        }, icon: Icon(Icons.add)), 
       ],
     );
 
-    if(asset is Descendant) {
+    if(widget.asset is Descendant) {
       stateHolder = Row(
         children: [
           Switch(
-            value: false,
+            value: owned[index],
             thumbIcon: thumbIconProperty,
-            onChanged: (value) {print("$value $index");}
+            onChanged: (value) {
+              setState(() {
+                owned[index] = value;
+              });
+              Utils.alert(context, "Saved!");
+            }
           ),
           SizedBox(width: 16)
         ],
@@ -93,8 +134,8 @@ class AssetDetails extends StatelessWidget {
           Expanded(
             flex: 25,
             child: Hero(
-              tag: asset.getName,
-              child: Center(child: Image.asset(asset.getImagePath))
+              tag: widget.asset.getName,
+              child: Center(child: Image.asset(widget.asset.getImagePath))
             ),
           ),
           Expanded(flex: 1, child: Center()),
@@ -112,8 +153,8 @@ class AssetDetails extends StatelessWidget {
               Expanded(
                 flex: 8,
                 child: Hero(
-                  tag: asset.getName,
-                  child: Center(child: Image.asset(asset.getImagePath))
+                  tag: widget.asset.getName,
+                  child: Center(child: Image.asset(widget.asset.getImagePath))
                 ),
               ),
               Expanded(flex: 1, child: Center()),
@@ -126,7 +167,7 @@ class AssetDetails extends StatelessWidget {
     
     return Scaffold(
       appBar: AppBar(
-        title: Text(asset.getName),
+        title: Text(widget.asset.getName),
       ),
       body: bodyWidget
     );
